3263d3e123812792f3e0562597964e3d15ca8ffb
Garv Rustagi <garv.rustagi@outlook.com>
2025-03-27 08:09:08 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+setup(
+    name="MedBot",
+    version="0.0.0",
+    author="Garv Rustagi",
+    author_email="garv.rustagi@outlook.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
